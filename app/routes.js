247311599f2c721a6e11f8ee34c2bf2fd2a52f24
Dan Sensecall <dan@sensecall.co.uk>
2020-11-27 12:05:05 +0000

    aed

--- app/routes.js
+++ app/routes.js
@@ -765,4 +765,18 @@ require('./routes/16-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
 require('./routes/17-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
 
 
+router.use(/\/aed-([0-9]+)-([0-9]+)/, (req, res, next) => {
+    req.version = req.originalUrl.split('/')[1]
+    req.session.data['prototypeVersion'] = req.version
+    
+    require(`./views/aed-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+})
+
+router.use(/\/provider-([0-9]+)-([0-9]+)/, (req, res, next) => {
+    req.version = req.originalUrl.split('/')[1]
+    req.session.data['prototypeVersion'] = req.version
+    
+    require(`./views/provider-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+})
+
 module.exports = router
